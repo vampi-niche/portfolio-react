c6bfc4f10df48964759972f43ea1c26027b6645a
vampi-niche <vampi.niche@gmail.com>
2024-04-18 23:58:32 -0400

    clickable

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,7 +20,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href=""
+                href="https://github.com/vampi-niche"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
